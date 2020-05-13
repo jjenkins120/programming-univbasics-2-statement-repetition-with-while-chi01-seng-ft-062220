1c0ede27eb8d03d2fb817c573ad6f1ef6ab9511d
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-13 20:35:31 +0000

    Automatically backed up by Learn

--- demo.rb
+++ demo.rb
@@ -1,5 +1,7 @@
-count = 0 
-while count <3 do
-  puts "I am the #{count}, I love to count!"
-  count += 1 
+count = 10 
+while count >= 1 do
+  puts "#{count}"
+  count = count - 1 
+  break if count == 0
 end
+puts "Happy New Year!"
