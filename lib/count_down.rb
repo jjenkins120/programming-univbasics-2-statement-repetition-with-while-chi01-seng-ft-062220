dbe34b8c040b1daad910fea5f2ad877a34de05ad
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-13 20:11:13 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,5 +1,8 @@
 # Write your code here
 count = 10 
-while count < 0 && count <= 10
-do
+while count < 0 && count <= 10 do
   puts "#{count}"
+  break if count == 0 
+  puts "Happy New Year!"
+  count -= 1 
+end
